37e690c49c04853e97cc075c6cc9a387654316f2
latas-dev <thelonelysuric@gmail.com>
2024-03-09 19:06:11 -0600

    changed linked_list.c

--- Data Structures/linked_list.c
+++ Data Structures/linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> // malloc
+#include <string.h>    // strcmp
+#include <stdlib.h>     // malloc
 
 typedef struct Promedio Promedio;
 struct Promedio {
@@ -7,8 +8,35 @@ struct Promedio {
     int  value;
     Promedio *next;
 };
+// Promedio methods 
+Promedio *newPromedio(char *name, int value);
+Promedio *addToFront(Promedio *list, Promedio *new_promedio);
+Promedio *addToBack(Promedio *list, Promedio *new_promedio);
+void lookupPromedio(Promedio *list, char *name);
+int listLength(Promedio *list);
+void deleteList(Promedio *list);
+// void map(Promedio *list, void (*fn)(Promedio *, void*), void *arg);
+// void printPromedio(Promedio *promedio, void *arg);
 
-Node *newPromedio(char *name, int value) {
+int main(int argc, char* argv[]) {
+    Promedio *clase = newPromedio("Pedro", 98);
+    clase = addToFront(clase, newPromedio("Andres", 90));
+    clase = addToBack(clase, newPromedio("Miguel", 70));
+
+
+    lookupPromedio(clase, "Juan");
+
+    Promedio *temp = clase;
+    for(; temp != NULL; temp = temp->next) {
+        printf("%s: %d", temp->name, temp->value);
+    }
+
+    deleteList(clase);
+    
+    return 0;
+}
+
+Promedio *newPromedio(char *name, int value) {
     Promedio *new_promedio;
     new_promedio = (Promedio*)malloc(sizeof(Promedio));
 
@@ -18,12 +46,12 @@ Node *newPromedio(char *name, int value) {
     return new_promedio;
 }
 
-Node *addToFront(Promedio *list, Promedio new_promedio) {
+Promedio *addToFront(Promedio *list, Promedio *new_promedio) {
     new_promedio->next = list;
     return new_promedio;
 }
 
-Node *addToBack(Promedio *list, Promedio new_promedio) {
+Promedio *addToBack(Promedio *list, Promedio *new_promedio) {
     Promedio *current;
 
     if(list == NULL) return new_promedio;
@@ -34,8 +62,33 @@ Node *addToBack(Promedio *list, Promedio new_promedio) {
     return current;
 }
 
-int main(int argc, char* argv[]) {
-
-
-    return 0;
+void lookupPromedio(Promedio *list, char *name) {
+    for(; list != NULL; list = list->next) {
+        if(!strcmp(name, list->name)) printf("%s: %d\n", list->name, list->value);
+    }
+    printf("Item not found\n");
 }
+
+int listLength(Promedio *list) {
+    int i = 0;
+
+    for(; list != NULL; list = list->next) {
+        i++;
+    }
+    return i;
+}
+
+void deleteList(Promedio *list) {
+    Promedio *next;
+    for(; list != NULL; list = next) {
+        next = list->next;
+        free(list);
+    }
+}
+
+
+// void map(Promedio *list, void (*fn)(Promedio *, void*), void *arg) {
+//     for(; list != NULL; list = list->next) {
+//         (*fn)(list, arg);
+//     }
+// }
